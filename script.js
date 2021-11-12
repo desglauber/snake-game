3cc76330a5f01419b41bdcb6466b2c79e30ac9bc
Glauber Farias <desglauber@gmail.com>
2021-11-12 16:39:17 -0400

    Definir fim do jogo

--- script.js
+++ script.js
@@ -40,9 +40,16 @@ function update(event){
 
 function iniciarJogo(){
     if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-    if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if (snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-    if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+    if (snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+
+    for (i = 1; i < snake.length; i++){
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(')
+        }
+    }
 
     criarBG();
     criarCobrinha();
@@ -57,14 +64,13 @@ function iniciarJogo(){
     if(direction == "down") snakeY += box;
 
     if (snakeX != food.x || snakeY != food.y){
-        snake.pop;
-    }
-    else{
+        snake.pop();
+    }else{
         food.x = Math.floor(Math.random() * 15 + 1) * box;
         food.y = Math.floor(Math.random() * 15 + 1) * box;
     }
 
-    snake.pop();
+    //snake.pop();
 
     let newHead = {
         x: snakeX,
